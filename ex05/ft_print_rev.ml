7369ffeb9047e303fdaa39f4774708b3fe5101a5
erobert <etiennerobert33@gmail.com>
2019-10-08 18:01:34 +0200

    ex05

---
+++ ex05/ft_print_rev.ml
@@ -0,0 +1,13 @@
+let ft_print_rev s =
+  let rec ft_print_rev_r s i =
+    if i >= 0 then (
+      print_char s.[i];
+      ft_print_rev_r s (i - 1)
+    )
+    in
+  ft_print_rev_r s (String.length s - 1);
+  print_char '\n'
+
+let () =
+  ft_print_rev "Hello World !";
+  ft_print_rev ""
