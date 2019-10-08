d4e1f8d1d6f71f4aa9379ae0fafc89e368505249
erobert <etiennerobert33@gmail.com>
2019-10-08 15:48:34 +0200

    add tests to ex00

--- ex00/ft_test_sign.ml
+++ ex00/ft_test_sign.ml
@@ -2,4 +2,8 @@ let ft_test_sign n =
   if n < 0 then
     Printf.printf "negative\n"
   else
-    Printf.printf "positive\n"
+    Printf.printf "positive\n" in
+
+ft_test_sign 42;
+ft_test_sign 0;
+ft_test_sign (-42);
