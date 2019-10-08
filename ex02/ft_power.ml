fda7e2a77b60ad7f9bef1466b6d4c8c423474dd8
erobert <etiennerobert33@gmail.com>
2019-10-08 16:42:32 +0200

    put recursive function inside

--- ex02/ft_power.ml
+++ ex02/ft_power.ml
@@ -1,8 +1,8 @@
-let rec ft_power_r x n r =
-  if n == 0 then r
-  else ft_power_r x (n - 1) (r * x) in
-
-let ft_power x n = ft_power_r x n 1 in
+let ft_power x n =
+  let rec ft_power_r x n r =
+    if n == 0 then r
+    else ft_power_r x (n - 1) (r * x) in
+  ft_power_r x n 1 in
 
 Printf.printf "%d\n" (ft_power 2 4);
 Printf.printf "%d\n" (ft_power 3 0);
