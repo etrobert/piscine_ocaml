95d5affd6f1f23bda2f2537dacfee9b673f94c22
erobert <etiennerobert33@gmail.com>
2019-10-09 00:09:40 +0200

    ex08

--- ex08/ft_rot_n.ml
+++ ex08/ft_rot_n.ml
@@ -1,17 +1,21 @@
-(* TODO not done *)
-
 let ft_rot_n n s =
   (*
-    The function f takes a char,
-    transforms it to a 'simple char', a mapping from 'a'..'z' to 0..25
-    applies the transformation ( + n % az_range )
+    The function rot_chr takes a char,
+    transforms it to a 'simple char', a mapping from min..max to 0..max-min
+    applies the transformation ( + n % range_size )
     then transforms it back to a normal char
   *)
   let f c =
-    let az_range = Char.code 'z' - Char.code 'a' in
-    let to_simple c = Char.code c - Char.code 'a' in
-    let from_simple s = Char.chr (s + Char.code 'a') in
-    from_simple ((to_simple c + n) mod az_range) in
+    let rot_chr min max c =
+      let range_size = Char.code max - Char.code min + 1 in
+      let to_simple c = Char.code c - Char.code min in
+      let from_simple s = Char.chr (s + Char.code min) in
+      from_simple ((to_simple c + n) mod range_size) in
+    let in_range min max x = x >= min && x <= max in
+    match c with
+      | c when in_range 'a' 'z' c -> rot_chr 'a' 'z' c
+      | c when in_range 'A' 'Z' c -> rot_chr 'A' 'Z' c
+      | c -> c in
   String.map f s
 
 let test_rot_n n s =
@@ -20,4 +24,8 @@ let test_rot_n n s =
 let () =
   test_rot_n 1 "abcdefghijklmnopqrstuvwxyz";
   test_rot_n 13 "abcdefghijklmnopqrstuvwxyz";
-  test_rot_n 42 "0123456789"
+  test_rot_n 42 "0123456789";
+  test_rot_n 2 "OI2EAS67B9";
+  test_rot_n 0 "Damned !";
+  test_rot_n 42 "";
+  test_rot_n 1 "NBzlk qnbjr !"
