66b59165530193b812e642c4c3a4fa24d8c10164
erobert <etiennerobert33@gmail.com>
2019-10-14 21:56:18 +0200

    update ex09 to match the subject requirements

--- d00/ex09/ft_print_comb2.ml
+++ d00/ex09/ft_print_comb2.ml
@@ -1,19 +1,23 @@
-let ft_print_comb2 =
+let ft_print_comb2 () =
   let rec ft_print_comb2_r comb =
     let next_comb = function
       | (a,99) -> a + 1, a + 2
       | (a,b) -> a, b + 1
       in
     let print_comb (a,b) =
-      Printf.printf "%02d %02d" a b
+      let print_number k = if k < 10 then print_char '0'; print_int k in
+      print_number a;
+      print_char ' ';
+      print_number b
       in
     print_comb comb;
     if comb <> (98,99) then (
-      print_string ", ";
+      print_char ',';
+      print_char ' ';
       ft_print_comb2_r (next_comb comb)
     )
     in
-    fun () -> ft_print_comb2_r (0,1);
+    ft_print_comb2_r (0,1);
     print_char '\n'
 
 let () =
