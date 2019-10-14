d4dac47c70de02b6ec95c69554023ffc8000df44
erobert <etiennerobert33@gmail.com>
2019-10-14 12:25:08 +0200

    remove print_string in ex01

--- ex01/ft_countdown.ml
+++ ex01/ft_countdown.ml
@@ -1,9 +1,12 @@
 let rec ft_countdown n =
-  if n <= 0 then print_string "0\n"
+  let print_value n =
+    print_int n;
+    print_char '\n'
+    in
+  if n <= 0 then print_value 0
   else
     begin
-      print_int n;
-      print_char '\n';
+      print_value n;
       ft_countdown (n - 1)
     end
 
