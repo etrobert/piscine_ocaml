9f7518357c558161dcfa87b8550f871283506604
erobert <etiennerobert33@gmail.com>
2019-10-15 18:27:02 +0200

    add tests to d01ex08

--- d01/ex08/ft_sum.ml
+++ d01/ex08/ft_sum.ml
@@ -3,5 +3,18 @@ let ft_sum e lb ub =
     match e, lb, ub, acc with
     | _, lb, ub, acc when lb > ub -> acc
     | e, lb, ub, acc -> loop e (lb + 1) ub (acc +. (e lb))
-    in
+  in
   if lb > ub then nan else loop e lb ub 0.0
+
+let () =
+  let test_square ((lb, ub), output) =
+    let r = ft_sum (fun i -> float_of_int (i * i)) lb ub in
+    Printf.printf (if r = output then "SUCCESS" else "FAILURE");
+    Printf.printf ": ft_sum (fun i -> float_of_int (i * i) %d %d = %f" lb ub r;
+    if r <> output then Printf.printf " <> %f\n" output else print_char '\n'
+  in
+  let tests_square = [
+    (1, 10), 385.0;
+    (1, 2), 5.0;
+  ] in
+  List.iter test_square tests_square
