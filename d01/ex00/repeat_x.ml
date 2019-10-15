1555d26ad8ec708968e07dc6a71f7620e82609d6
erobert <etiennerobert33@gmail.com>
2019-10-15 16:48:48 +0200

    add tests to d01ex00

--- d01/ex00/repeat_x.ml
+++ d01/ex00/repeat_x.ml
@@ -2,3 +2,20 @@ let rec repeat_x = function
   | n when n < 0 -> "Error"
   | 0 -> ""
   | n -> repeat_x (n - 1) ^ "x"
+
+let () =
+  let test (input, output) =
+    let r = repeat_x input in
+    if r = output
+    then Printf.printf "SUCCESS: repeat_x %d = \"%s\"\n" input output
+    else
+      Printf.printf "FAILURE: repeat_x %d = \"%s\" <> \"%s\"\n" input r output
+    in
+  let tests = [
+    -1, "Error";
+    0, "";
+    1, "x";
+    3, "xxx";
+    5, "xxxxx"
+  ] in
+  List.iter test tests
