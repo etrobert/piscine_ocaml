797fc8c36fe0a9fc219dc901c021336d3a2cef93
erobert <etiennerobert33@gmail.com>
2019-10-15 17:07:13 +0200

    add tests to d01ex02

--- d01/ex02/ackermann.ml
+++ d01/ex02/ackermann.ml
@@ -4,3 +4,19 @@ let rec ackermann m n =
   | 0, n -> n + 1
   | m, 0 -> ackermann (m - 1) 1
   | m, n -> ackermann (m - 1) (ackermann m (n - 1))
+
+let () =
+  let test ((m, n), output) =
+    let r = ackermann m n in
+    if r = output
+    then Printf.printf "SUCCESS: ackermann %d %d = %d\n" m n r
+    else
+      Printf.printf "FAILURE: ackermann %d %d = %d <> %d\n" m n r output
+    in
+  let tests = [
+    (-1, 7), (-1);
+    (0, 0), 1;
+    (2, 3), 9;
+    (4, 1), 65533
+  ] in
+  List.iter test tests
