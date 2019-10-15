93361916fe479f3e02bf41754489933fb7750387
erobert <etiennerobert33@gmail.com>
2019-10-15 17:00:15 +0200

    remove pattern matching from d01ex01

--- d01/ex01/repeat_string.ml
+++ d01/ex01/repeat_string.ml
@@ -1,8 +1,7 @@
 let rec repeat_string ?(str = "x") n =
-  match str,n with
-  | _,n when n < 0 -> "Error"
-  | _,0 -> ""
-  | s,n -> repeat_string ~str:str (n - 1) ^ s
+  if n < 0 then "Error"
+  else if n = 0 then ""
+  else repeat_string ~str:str (n - 1) ^ str
 
 let () =
   let test (input, output) =
