2f0076a2e88bea753296bd7a5a3bd1d4e3e7fc87
erobert <etiennerobert33@gmail.com>
2019-10-15 14:06:45 +0200

    improve ex06 tests

--- d00/ex06/ft_string_all.ml
+++ d00/ex06/ft_string_all.ml
@@ -9,5 +9,13 @@ let ft_string_all p s =
 
 let () =
   let is_digit c = c >= '0' && c <= '9' in
-  Printf.printf "%B\n" (ft_string_all is_digit "0123456789");
-  Printf.printf "%B\n" (ft_string_all is_digit "012345A789");
+  let test_ft_string_all_is_digit input output =
+    let r = ft_string_all is_digit input in
+    if r = output
+    then Printf.printf
+      "SUCCESS: ft_string_all is_digit \"%s\" = %B\n" input output
+    else Printf.printf
+      "FAILURE: ft_string_all is_digit \"%s\" = %B <> %B" input r output
+    in
+  test_ft_string_all_is_digit "0123456789" true;
+  test_ft_string_all_is_digit "012345A789" false
