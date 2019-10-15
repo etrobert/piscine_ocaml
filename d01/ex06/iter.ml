5aec4972d145581df779ba41cccb9bee7dd28b64
erobert <etiennerobert33@gmail.com>
2019-10-15 18:06:22 +0200

    add tests to d01ex06

--- d01/ex06/iter.ml
+++ d01/ex06/iter.ml
@@ -3,3 +3,35 @@ let rec iter p x n =
   | _, _, n when n < 0 -> -1
   | _, x, 0 -> x
   | p, x, n -> iter p (p x) (n - 1)
+
+let () =
+  let test_square ((x, n), output) =
+    let r = iter (fun x -> x * x) x n in
+    if r = output
+    then Printf.printf "SUCCESS: iter (fun x -> x * x) %d %d = %d\n" x n r
+    else
+      Printf.printf
+        "FAILURE: iter (fun x -> x * x) %d %d = %d <> %d\n" x n r output
+  in
+  let tests = [
+    (2, 4), 65536;
+    (2, (-1)), (-1);
+    (1, 5), 1;
+  ] in
+  List.iter test_square tests;
+
+  let test_double ((x, n), output) =
+    let r = iter (fun x -> x * 2) x n in
+    if r = output
+    then Printf.printf "SUCCESS: iter (fun x -> x * 2) %d %d = %d\n" x n r
+    else
+      Printf.printf
+        "FAILURE: iter (fun x -> x * 2) %d %d = %d <> %d\n" x n r output
+  in
+  let tests_double = [
+    (2, 4), 32;
+    (2, (-1)), (-1);
+    (1, 5), 32;
+  ] in
+  List.iter test_double tests_double
+
