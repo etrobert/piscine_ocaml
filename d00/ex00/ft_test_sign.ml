3be2d1031891393f003d302bdf7fb59bf1b8351c
erobert <etiennerobert33@gmail.com>
2019-10-15 12:42:14 +0200

    improve d00ex00 tests

--- d00/ex00/ft_test_sign.ml
+++ d00/ex00/ft_test_sign.ml
@@ -5,6 +5,11 @@ let ft_test_sign n =
     print_endline "positive"
 
 let () =
-  ft_test_sign 42;
-  ft_test_sign 0;
-  ft_test_sign (-42)
+  let test_ft_test_sign input output =
+    Printf.printf "ft_test_sign(%d) should give \"%s\" : " input output;
+    ft_test_sign input
+    in
+
+  test_ft_test_sign 42 "positive";
+  test_ft_test_sign 0 "positive";
+  test_ft_test_sign (-42) "negative";
