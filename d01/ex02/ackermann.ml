743c9ca88720365672def3924343f9c64e699798
erobert <etiennerobert33@gmail.com>
2019-10-15 17:07:54 +0200

    remove pattern matching from d01 ex02

--- d01/ex02/ackermann.ml
+++ d01/ex02/ackermann.ml
@@ -1,9 +1,8 @@
 let rec ackermann m n =
-  match m,n with
-  | m, n when m < 0 || n < 0 -> -1
-  | 0, n -> n + 1
-  | m, 0 -> ackermann (m - 1) 1
-  | m, n -> ackermann (m - 1) (ackermann m (n - 1))
+  if m < 0 || n < 0 then -1
+  else if m = 0 then n + 1
+  else if n = 0 then ackermann (m - 1) 1
+  else ackermann (m - 1) (ackermann m (n - 1))
 
 let () =
   let test ((m, n), output) =
