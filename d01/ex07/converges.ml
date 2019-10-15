2bd90b6a28a03193b71fa7c9dbe22061217eedff
erobert <etiennerobert33@gmail.com>
2019-10-15 18:19:01 +0200

    add tests to d01 ex07

--- d01/ex07/converges.ml
+++ d01/ex07/converges.ml
@@ -5,3 +5,28 @@ let rec converges f x n =
   | f, x, _ when x = f_x -> true
   | _, _, 0 -> false
   | f, x, n -> converges f f_x (n - 1)
+
+let () =
+  let test_times_two ((x, n), output) =
+    let r = converges (( * ) 2) x n in
+    Printf.printf (if r = output then "SUCCESS" else "FAILURE");
+    Printf.printf ": converges %d %d = %B" x n r;
+    if r <> output then Printf.printf " <> %B\n" output else print_char '\n'
+  in
+  let tests_times_two = [
+    (2, 5), false;
+  ] in
+  List.iter test_times_two tests_times_two;
+
+  let test_halve ((x, n), output) =
+    let r = converges (fun x -> x / 2) x n in
+    Printf.printf (if r = output then "SUCCESS" else "FAILURE");
+    Printf.printf ": converges %d %d = %B" x n r;
+    if r <> output then Printf.printf " <> %B\n" output else print_char '\n'
+  in
+  let tests_halve = [
+    (2, 3), true;
+    (2, 2), true;
+    (2, 1), false;
+  ] in
+  List.iter test_halve tests_halve
