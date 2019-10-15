979dd0792bf75c452d629e8bda9c521689d2101b
erobert <etiennerobert33@gmail.com>
2019-10-15 17:10:36 +0200

    add tests to d03ex03

--- d01/ex03/tak.ml
+++ d01/ex03/tak.ml
@@ -1,3 +1,20 @@
 let rec tak x y z =
   if y < x then tak (tak (x - 1) y z) (tak (y - 1) z x) (tak (z - 1) x y)
   else z
+
+let () =
+  let test ((x, y, z), output) =
+    let r = tak x y z in
+    if r = output
+    then Printf.printf "SUCCESS: ackermann %d %d %d = %d\n" x y z r
+    else
+      Printf.printf "FAILURE: ackermann %d %d %d = %d <> %d\n" x y z r output
+    in
+  let tests = [
+    (1, 2, 3), 3;
+    (5, 23, 7), 7;
+    (1, 1, 1), 1;
+    (0, 42, 0), 0;
+    (23498, 98734, 98776), 98776;
+  ] in
+  List.iter test tests
