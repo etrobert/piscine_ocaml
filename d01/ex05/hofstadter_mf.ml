b576809b8c307575cfc0e8cde0e8f7222b5f8779
erobert <etiennerobert33@gmail.com>
2019-10-15 17:43:02 +0200

    add tests to d01ex05

--- d01/ex05/hofstadter_mf.ml
+++ d01/ex05/hofstadter_mf.ml
@@ -7,3 +7,30 @@ and hfs_m = function
   | n when n < 0 -> -1
   | 0 -> 0
   | n -> n - hfs_f (hfs_m (n - 1))
+
+let () =
+  let test_m (input, output) =
+    let r = hfs_m input in
+    if r = output
+    then Printf.printf "SUCCESS: hfs_m %d = %d\n" input r
+    else
+      Printf.printf "FAILURE: hfs_m %d = %d <> %d\n" input r output
+    in
+  let tests_m = [
+    0, 0;
+    4, 2;
+  ] in
+  List.iter test_m tests_m;
+
+  let test_f (input, output) =
+    let r = hfs_f input in
+    if r = output
+    then Printf.printf "SUCCESS: hfs_f %d = %d\n" input r
+    else
+      Printf.printf "FAILURE: hfs_f %d = %d <> %d\n" input r output
+    in
+  let tests_f = [
+    0, 1;
+    4, 3;
+  ] in
+  List.iter test_f tests_f
