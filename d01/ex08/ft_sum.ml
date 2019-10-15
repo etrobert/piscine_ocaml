29e28b38cee19b87e3a9301a1df00c33567373bf
erobert <etiennerobert33@gmail.com>
2019-10-15 18:27:47 +0200

    remove pattern matching from d01ex08

--- d01/ex08/ft_sum.ml
+++ d01/ex08/ft_sum.ml
@@ -1,8 +1,7 @@
 let ft_sum e lb ub =
   let rec loop e lb ub acc =
-    match e, lb, ub, acc with
-    | _, lb, ub, acc when lb > ub -> acc
-    | e, lb, ub, acc -> loop e (lb + 1) ub (acc +. (e lb))
+    if lb > ub then acc
+    else loop e (lb + 1) ub (acc +. (e lb))
   in
   if lb > ub then nan else loop e lb ub 0.0
 
