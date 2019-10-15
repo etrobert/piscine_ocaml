df35d9cf60a810fbe195c602e9436472a33541d8
erobert <etiennerobert33@gmail.com>
2019-10-15 13:46:41 +0200

    improve d00ex02 tests

--- d00/ex02/ft_power.ml
+++ d00/ex02/ft_power.ml
@@ -5,6 +5,12 @@ let ft_power x n =
   ft_power_r x n 1
 
 let () =
-  Printf.printf "%d\n" (ft_power 2 4);
-  Printf.printf "%d\n" (ft_power 3 0);
-  Printf.printf "%d\n" (ft_power 0 5);
+  let test_ft_power (x, n) output =
+    let r = ft_power x n in
+    if r == output
+    then Printf.printf "SUCCESS: ft_power %d %d = %d\n" x n r
+    else Printf.printf "FAILURE: ft_power %d %d = %d <> %d\n" x n r output
+    in
+  test_ft_power (2,4) 16;
+  test_ft_power (3,0) 1;
+  test_ft_power (0,5) 0;
