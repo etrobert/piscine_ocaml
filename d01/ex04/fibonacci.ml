b4ce1f1be225a27b0b596d007884d2a707d01a6c
erobert <etiennerobert33@gmail.com>
2019-10-15 17:39:29 +0200

    add tests to d01 ex04

--- d01/ex04/fibonacci.ml
+++ d01/ex04/fibonacci.ml
@@ -6,3 +6,19 @@ let fibonacci n =
     in
   if n < 0 then (-1)
   else fibonacci_r 1 0 n
+
+let () =
+  let test (input, output) =
+    let r = fibonacci input in
+    if r = output
+    then Printf.printf "SUCCESS: fibonacci %d = %d\n" input r
+    else
+      Printf.printf "FAILURE: fibonacci %d = %d <> %d\n" input r output
+    in
+  let tests = [
+    (-42), (-1);
+    1, 1;
+    3, 2;
+    6, 8
+  ] in
+  List.iter test tests
