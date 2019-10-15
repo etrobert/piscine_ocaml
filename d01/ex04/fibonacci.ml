d4afdd8537c2f9197908078b0b10743133e19932
erobert <etiennerobert33@gmail.com>
2019-10-15 17:39:55 +0200

    use if else instead of pattern matching in d01 ex04

--- d01/ex04/fibonacci.ml
+++ d01/ex04/fibonacci.ml
@@ -1,8 +1,7 @@
 let fibonacci n =
   let rec fibonacci_r a b n =
-    match a,b,n with
-    | _, b, 0 -> b
-    | a, b, n -> fibonacci_r (a + b) a (n - 1)
+    if n = 0 then b
+    else fibonacci_r (a + b) a (n - 1)
     in
   if n < 0 then (-1)
   else fibonacci_r 1 0 n
