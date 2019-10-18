a0050e11fef1c22021c01b023a720926135ad69b
erobert <etiennerobert33@gmail.com>
2019-10-18 17:50:55 +0200

    finished d03ex03

--- d03/ex03/btree.ml
+++ d03/ex03/btree.ml
@@ -38,6 +38,25 @@ let rec search_bst v t =
   | Node (x, _, r) when x > v -> search_bst v r
   | Node (x, _, _) -> true
 
+let rec add_bst v = function
+  (* The first case should not be checked recursively *)
+  | Nil -> Node (v, Nil, Nil)
+  | Node (x, Nil, r) when v < x -> Node (x, Node (v, Nil, Nil), r)
+  | Node (x, l, r) when v < x -> Node (x, add_bst v l, r)
+  | Node (x, l, Nil) -> Node (x, l, Node (v, Nil, Nil))
+  | Node (x, l, r) -> Node (x, l, add_bst v r)
+
+(* Multiple of the same value are not handled *)
+let rec delete_bst v = function
+  | Nil -> failwith "The item to delete could not be found"
+  | Node (x, l, r) when x = v ->
+let rec rotate l r = match l with
+    | Nil -> r
+    | Node (x, ll, lr) -> Node (x, rotate ll lr , r)
+    in
+  rotate l r
+  | Node (x, l, r) -> Node (x, delete_bst x l, delete_bst x r)
+
 (* Tests *)
 
 let rec repeat f = function 0 -> () | n -> f (); repeat f (n - 1)
@@ -45,16 +64,20 @@ let rec repeat f = function 0 -> () | n -> f (); repeat f (n - 1)
 let print_tree printer t =
   let print_pre pre = repeat (fun () -> print_char '-') pre in
   let rec loop pre t =
-    match t with
-    | Nil -> ()
-    | Node (x, left, right) ->
-      begin
+    let print_node pre v =
         print_pre pre;
-        printer x;
-        print_char '\n';
-        loop (pre + 1) left;
-        loop (pre + 1) right
-      end
+        printer v;
+        print_char '\n'
+        in
+    match t with
+    | Node (x, Nil, Nil) -> print_node pre x
+    | Nil ->
+      print_pre pre;
+      print_char '\n'
+    | Node (x, left, right) ->
+      print_node pre x;
+      loop (pre + 1) left;
+      loop (pre + 1) right
   in
   loop 0 t
 
@@ -128,5 +151,29 @@ let () =
   test_search_bst "empty" empty 2;
   test_search_bst "bst" bst 1;
   test_search_bst "big_perfect_bst" big_perfect_bst 1;
-  test_search_bst "not_balanced" not_balanced 1
+  test_search_bst "not_balanced" not_balanced 1;
 
+  let test_add_bst name tree item =
+    print_endline "******";
+    print_tree print_int tree;
+    print_string ("add_bst " ^ string_of_int item ^ " " ^ name ^ " =\n");
+    print_tree print_int (add_bst item tree)
+    in
+  test_add_bst "empty" empty 2;
+  test_add_bst "bst" bst 1;
+  test_add_bst "big_perfect_bst" big_perfect_bst 1;
+  test_add_bst "not_balanced" not_balanced 1;
+
+  let test_delete_bst name tree item =
+    print_endline "******";
+    print_tree print_int tree;
+    try
+      print_string ("delete_bst " ^ string_of_int item ^ " " ^ name ^ " =\n");
+      print_tree print_int (delete_bst item tree)
+    with e -> print_endline (Printexc.to_string e)
+    in
+  test_delete_bst "empty" empty 2;
+  test_delete_bst "bst" bst 1;
+  test_delete_bst "big_perfect_bst" big_perfect_bst 1;
+  test_delete_bst "big_perfect_bst" big_perfect_bst 5;
+  test_delete_bst "not_balanced" not_balanced 1
